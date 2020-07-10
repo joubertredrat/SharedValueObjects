c040a9137aeca65ef94267d032acc9b9b6230142
Joubert Guimarães de Assis <eu+github@redrat.com.br>
2020-07-10 00:41:48 -0300

    Add landline value object

---
+++ src/ValueObject/Brazil/Telecom/Landline.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RedRat\SharedValueObjects\ValueObject\Brazil\Telecom;
+
+use RedRat\SharedValueObjects\Exception\ValueObject\Brazil\Telecom\Landline\InvalidNumberException;
+
+use function is_null;
+use function sprintf;
+use function substr;
+
+final class Landline
+{
+    private string $number;
+
+    public function __construct(string $number)
+    {
+        if (!self::isValid($number)) {
+            throw InvalidNumberException::handle($number);
+        }
+
+        $this->number = $number;
+    }
+
+    public function getNumber(): string
+    {
+        return $this->number;
+    }
+
+    public function getNumberMasked(): string
+    {
+        return sprintf(
+            '%s-%s',
+            substr($this->getNumber(), 0, 4),
+            substr($this->getNumber(), -4)
+        );
+    }
+
+    public static function isValid(?string $number): bool
+    {
+        if (is_null($number)) {
+            return false;
+        }
+
+        return false !== filter_var(
+            $number,
+            FILTER_VALIDATE_REGEXP,
+            ['options' => ['regexp' => '/^([1-5]{1}[\d]{7})$/']]
+        );
+    }
+}
---
+++ tests/Unit/ValueObject/Brazil/Telecom/LandlineTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RedRat\SharedValueObjects\Tests\Unit\ValueObject\Brazil\Telecom;
+
+use PHPUnit\Framework\TestCase;
+use RedRat\SharedValueObjects\Exception\ValueObject\Brazil\Telecom\Landline\InvalidNumberException;
+use RedRat\SharedValueObjects\ValueObject\Brazil\Telecom\Landline;
+
+class LandlineTest extends TestCase
+{
+    public function testAttributes(): void
+    {
+        $landlineNumberExpected = '28101123';
+        $landlineNumberMaskedExpected = '2810-1123';
+        $landline = new Landline($landlineNumberExpected);
+
+        self::assertEquals($landlineNumberExpected, $landline->getNumber());
+        self::assertEquals($landlineNumberMaskedExpected, $landline->getNumberMasked());
+    }
+
+    public function testConstructThrowInvalidNumberException(): void
+    {
+        self::expectException(InvalidNumberException::class);
+
+        new Landline('other');
+    }
+
+    public function testIsValid(): void
+    {
+        self::assertTrue(Landline::isValid('28101123'));
+    }
+
+    public function testIsNotValidNull(): void
+    {
+        self::assertFalse(Landline::isValid(null));
+    }
+
+    public function testIsNotValidCode(): void
+    {
+        self::assertFalse(Landline::isValid('78101123'));
+    }
+}
